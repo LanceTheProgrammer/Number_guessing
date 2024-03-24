4e2fb29f00af82f00fb3aa68c14f35710da8d200
Lance Pruitt <ljpruitt123@gmail.com>
2024-03-24 19:18:13 +0000

    feat: query games and get username

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+PSQL ="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
+echo "Enter your username:"
+read USERNAME
+
+RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = $USERNAME")
+
+if [[ -z $RETURNING_USER ]]
+then
+  INSERTED_USER=$(PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
+else
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
+BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
+echo GAMES_PLAYED
+echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+
+fi
+
+
+echo $(( $RANDOM % 1000 + 1 ))
