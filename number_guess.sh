092c8419fe6c5f4a500379cfe8a51b1c2de2df17
Lance Pruitt <ljpruitt123@gmail.com>
2024-03-24 19:25:07 +0000

    feat: add guess skeleton

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,7 @@
 
 PSQL ="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
 echo "Enter your username:"
+SECRET_NUMBER=$(( $RANDOM % 1000 + 1 ))
 read USERNAME
 
 RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = $USERNAME")
@@ -10,7 +11,7 @@ if [[ -z $RETURNING_USER ]]
 then
   INSERTED_USER=$(PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
   echo Welcome, $USERNAME! It looks like this is your first time here.
-  
+
 else
 GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
 BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
@@ -19,5 +20,26 @@ echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best
 
 fi
 
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
 
-echo $(( $RANDOM % 1000 + 1 ))
+if [[ ! $GUESS =~ ^[0-9]+$ ]]
+then
+
+else
+
+
+  if [[ $GUESS -gt $SECRET_NUMBER]]
+
+  then
+  elif [[ $GUESS -lt $SECRET_NUMBER ]]
+  then
+
+  else
+
+  fi
+
+  
+fi
+
+
