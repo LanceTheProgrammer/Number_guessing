95e2ddfdcea84457fc80b0443641b70e4055aa8a
Lance Pruitt <ljpruitt123@gmail.com>
2024-03-24 19:45:44 +0000

    fix: smooth out bugs

--- number_guess.sh
+++ number_guess.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PSQL ="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
 echo "Enter your username:"
 SECRET_NUMBER=$(( $RANDOM % 1000 + 1 ))
 read USERNAME
 
-RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = $USERNAME")
+RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
 
 if [[ -z $RETURNING_USER ]]
 then
@@ -13,20 +13,21 @@ then
   echo Welcome, $USERNAME! It looks like this is your first time here.
 
 else
-GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
-BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
-echo GAMES_PLAYED
-echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN users USING(user_id) WHERE username = $USERNAME")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN users USING(user_id) WHERE username = $USERNAME")
+  echo GAMES_PLAYED
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
 fi
 
+echo "$SECRET_NUMBER"
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
 TRIES=1
 while [ ! $GUESS -eq $SECRET_NUMBER ]
 do
 
-  TRIES=$TRIES+1
+  TRIES=$(expr $TRIES + 1)
 
   if [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
@@ -34,7 +35,7 @@ do
     read GUESS
   else
 
-    if [[ $GUESS -gt $SECRET_NUMBER]]
+    if [[ $GUESS -gt $SECRET_NUMBER ]]
     then
 
     echo "It's higher than that, guess again:"
